065905603135e06506a0b82aa61c4156a4268ea6
Omri Elad <omlad93@gmail.com>
2021-10-17 11:28:27 +0300

    operation files update

--- sim/sim/opp.c
+++ sim/sim/opp.c
@@ -15,17 +15,9 @@ set the functions to be performed in the *op_code field in operation struct
 */
 void* set_op_by_code(int code, operation* op) {
 
-	//if operations should do nothing (on assignment to $0 )
-	if (((code <= 8) || (code == 16) || (code == 19))) {
-		if (op->rd == 0) {
-			return &nop;
-		}
-	}
-	else if ((op->rd > 15) || (op->rs > 15) || (op->rt > 15)) {
+	if (code <=8 && op->rd == 0) {
 		return &nop;
 	}
-
-
 	//else go by opcode
 	switch (code) {
 	case(0):
@@ -33,114 +25,94 @@ void* set_op_by_code(int code, operation* op) {
 	case(1):
 		return &sub;
 	case(2):
-		return &and;
+		return &lsf;
 	case(3):
-		return &or ;
+		return &rsf;
 	case(4):
-		return &xor;
+		return &and;
 	case(5):
-		return &mul;
+		return &or;
 	case(6):
-		return &sll;
+		return &xor;
 	case(7):
-		return &sra;
+		return &lhi;
 	case(8):
-		return &srl;
+		return &ld;
 	case(9):
-		return &beq;
-	case(10):
-		return &bne;
-	case(11):
-		return &blt;
-	case(12):
-		return &bgt;
-	case(13):
-		return &ble;
-	case(14):
-		return &bge;
-	case(15):
-		return &jal;
+		return &st;
 	case(16):
-		return &lw;
+		return &jlt;
 	case(17):
-		return &sw;
+		return &jle;
 	case(18):
-		return &reti;
+		return &jeq;
 	case(19):
-		return &in;
+		return &jne;
 	case(20):
-		return &out;
-	case(21):
+		return &jin;
+	case(24):
 		return &halt;
-		//if unknown opcode -> do nop
 	default:
 		return &nop;
 	}
 }
 
 /*
-return the IO-register name by it's number
+set the function name in operation struct
 */
-char* IO_reg_name(int reg_num) {
-	switch (reg_num) {
+void set_op_name_by_code(int code, operation* op) {
+	//else go by opcode
+	switch (code) {
 	case(0):
-		return "irq0enable";
+		op->op_name = "ADD";
+		return;
 	case(1):
-		return "irq1enable";
+		op->op_name = "SUB";
+		return;
 	case(2):
-		return "irq2enable";
+		op->op_name = "LSF";
+		return;
 	case(3):
-		return "irq0status";
+		op->op_name = "RSF";
+		return;
 	case(4):
-		return "irq1status";
+		op->op_name = "AND";
+		return;
 	case(5):
-		return "irq2status";
+		op->op_name = "OR";
+		return;
 	case(6):
-		return "irqhandler";
+		op->op_name = "XOR";
+		return;
 	case(7):
-		return "irqreturn";
+		op->op_name = "LHI";
+		return;
 	case(8):
-		return "clks";
+		op->op_name = "LD";
+		return;
 	case(9):
-		return "leds";
-	case(10):
-		return "reserved";
-	case(11):
-		return "timerenable";
-	case(12):
-		return "timercurrent";
-	case(13):
-		return "timermax";
-	case(14):
-		return "diskcmd";
-	case(15):
-		return "disksector";
+		op->op_name = "ST";
+		return;
 	case(16):
-		return "diskbuffer";
+		op->op_name = "JLT";
+		return;
 	case(17):
-		return("diskstatus");
+		op->op_name = "JLE";
+		return;
 	case(18):
-		return("monitorcmd");
+		op->op_name = "JEQ";
+		return;
 	case(19):
-		return("monitorx");
+		op->op_name = "JNE";
+		return;
 	case(20):
-		return("monitory");
-	case(21):
-		return("monitordata");
+		op->op_name = "JIN";
+		return;
+	case(24):
+		op->op_name = "HLT";
+		return;
 	default:
-		return "";
-	}
-}
-
-/*
- return 1 if an immediate is used in the operation
- */
-int imm_usage(operation* op) {
-	if (op->rs == 1 || op->rd == 1 || op->rt == 1) {
-		return 1;
-	}
-	else {
-		return  0;
+		op->op_name = "NOP";
 	}
 }
 
@@ -149,330 +121,138 @@ load input values to fields in the operation struct
 */
 void set_operation(operation* op, int d, int t, int s, int code, char* inst) {
 	op->rd = d;
-	op->rt = t;
-	op->rs = s;
+	op->src1 = t;
+	op->src0 = s;
+	op->op_num = code;
 	op->op_code = set_op_by_code(code, op);
 	op->inst = inst;
 	op->imm_used = imm_usage(op);
+	set_op_name_by_code(code,op);
 
 }
 
-/*
-copies memory from one memory elemnt to enother
-used to copy from Data Memory to Hard Disk or vise-versa
-*/
-void mem_copy(int *origin, int* dest) {
-	int j;
-	for (j = 0; j < 128; j++) { //copy sector
-		dest[j] = origin[j];
-	}
-	IOs[17] = 1; //set HD as busy
-}
-
-/*
-A function to make sure that mem copy between Disk & Data-Mem will go clean
-if one of the pointer is set to an addresss which will cause a problem - it is shifter to the mximum address which won't
-to be called before mem_copy
-*/
-void fix_disk_cpy(unsigned int disk_sec, unsigned int dmem_loc, int* dmem_address, int* disk_address) {
-	int max_dmem_to_cpy = 4096 - 128;
-	if (dmem_loc > max_dmem_to_cpy) {
-		dmem_address = &Data_Mem[max_dmem_to_cpy];
-	}
-	if (disk_sec > 127) {
-		disk_address = Disk[127];
-	}
-}
 
 /* **********************************************************/
 /*  ~~~~~~~~~~~~~    SIMP OP CODES OPERATIONS ~~~~~~~~~~~~  */
 /* **********************************************************/
-//1
+//0
 int add(operation* op, int pc) {
-	REG[op->rd] = REG[op->rs] + REG[op->rt];
-	if (op->imm_used) {
-		return pc + 2;
-	}
+	REG[op->rd] = REG[op->src0] + REG[op->src1];
+	return (pc + 1);
+
+}
+
+//1
+int sub(operation* op, int pc) {
+	REG[op->rd] = REG[op->src0] - REG[op->src1];
 	return (pc + 1);
 
 }
 
 //2
-int sub(operation* op, int pc) {
-	REG[op->rd] = REG[op->rs] - REG[op->rt];
-	if (op->imm_used) {
-		return pc + 2;
-	}
+int lsf(operation* op, int pc) {
+	REG[op->rd] = REG[op->src0] << REG[op->src1];
 	return (pc + 1);
-
 }
 
 //3
-int and(operation* op, int pc) {
-	REG[op->rd] = REG[op->rs] & REG[op->rt];
-	if (op->imm_used) {
-		return pc + 2;
-	}
+int rsf (operation* op, int pc) {
+	REG[op->rd] = REG[op->src0] >> REG[op->src1];
 	return (pc + 1);
-
 }
 
 //4
-int or (operation* op, int pc) {
-	REG[op->rd] = REG[op->rs] | REG[op->rt];
-	if (op->imm_used) {
-		return pc + 2;
-	}
+int and (operation* op, int pc) {
+	REG[op->rd] = REG[op->src0] & REG[op->src1];
 	return (pc + 1);
 
 }
 
 //5
-int xor (operation* op, int pc) {
-	REG[op->rd] = REG[op->rs] ^ REG[op->rt];
-	if (op->imm_used) {
-		return pc + 2;
-	}
+int or(operation* op, int pc) {
+	REG[op->rd] = REG[op->src0] | REG[op->src1];
 	return (pc + 1);
-
 }
 
 //6
-int mul(operation* op, int pc) {
-	REG[op->rd] = REG[op->rs] * REG[op->rt];
-	if (op->imm_used) {
-		return pc + 2;
-	}
-	return (pc + 1);
-
-}
-
-//6
-int sll(operation* op, int pc) {
-	REG[op->rd] = REG[op->rs] << REG[op->rt];
-	if (op->imm_used) {
-		return pc + 2;
-	}
+int xor(operation* op, int pc) {
+	REG[op->rd] = REG[op->src0] ^ REG[op->src1];
 	return (pc + 1);
 }
 
 //7
-int sra(operation* op, int pc) {
-	REG[op->rd] = REG[op->rs] >> REG[op->rt];
-	if (op->imm_used) {
-		return pc + 2;
-	}return (pc + 1);
+int lhi(operation* op, int pc) {
+	int low_bits = op->rd && LOW_MASK;
+	int shifted_imm = REG[1] << HIG_SHFT;
+	REG[op->rd] = low_bits | shifted_imm;
+	return (pc + 1);
 }
 
 //8
-int srl(operation* op, int pc) {
-	REG[op->rd] = (int)REG[op->rs] >> (int)REG[op->rt];
-	if (op->imm_used) {
-		return pc + 2;
-	}return (pc + 1);
+int ld(operation* op, int pc) {
+	REG[op->rd] = MEM[REG[op->src1]]];
+	return (pc + 1);
 }
 
 //9
-int beq(operation* op, int pc) {
-	if (REG[op->rs] != REG[op->rt]) {
-		if (op->imm_used) {
-			return pc + 2;
-		}
+int st(operation* op, int pc) {
+		MEM[REG[op->src1]] = REG[op->src0]
 		return (pc + 1);
-	}
-
-	return (REG[op->rd] & LSB_9BIT);
 }
 
-//10
-int bne(operation* op, int pc) {
-	if (REG[op->rs] == REG[op->rt]) {
-		if (op->imm_used) {
-			return pc + 2;
-		}
-		return (pc + 1);
-	}
 
-	return (REG[op->rd] & LSB_9BIT);
-}
-
-//11
-int blt(operation* op, int pc) {
-
-	if (REG[op->rs] >= REG[op->rt]) {
-		if (op->imm_used) {
-			return pc + 2;
-		}
-		return (pc + 1);
-	}
-
-	return  (REG[op->rd] & LSB_9BIT);
-}
-
-//12
-int bgt(operation* op, int pc) {
-	if (REG[op->rs] <= REG[op->rt]) {
-		if (op->imm_used) {
-			return pc + 2;
-		}
-		return (pc + 1);
-	}
-
-	return (REG[op->rd] & LSB_9BIT);
-}
-
-//13
-int ble(operation* op, int pc) {
-	if (REG[op->rs] > REG[op->rt]) {
-		if (op->imm_used) {
-			return pc + 2;
-		}
-		return (pc + 1);
-	}
-	return (REG[op->rd] & LSB_9BIT);
-}
-
-//14
-int bge(operation* op, int pc) {
-	if (REG[op->rs] < REG[op->rt]) {
-		if (op->imm_used) {
-			return pc + 2;
-		}
-		return (pc + 1);
-	}
-	return (REG[op->rd] & LSB_9BIT);
-}
-
-//15
-int jal(operation* op, int pc) {
-	if (op->imm_used) {
-		REG[RA] = pc + 2;
-	}
-	else {
-		REG[RA] = pc + 1;
-	}
-	return (REG[op->rd] & LSB_9BIT);
-}
 
 //16
-int lw(operation* op, int pc) {
-	//char value[MAX_LINE + 1];
-	int memory_line = (REG[op->rs] + REG[op->rt]);
-	if (memory_line > 4095 || memory_line < 0) {
-		memory_line = 4095; //do not allow higher addresses 
-	}	REG[op->rd] = Data_Mem[memory_line];
-	if (op->imm_used) {
-		return pc + 2;
+int jlt(operation* op, int pc) {
+	if (REG[op->src0] < REG[op->src1]) {
+		REG[7] = pc;
+		return REG[1];
 	}
-	return (pc + 1);
+		return (pc + 1);
 }
 
 //17
-int sw(operation* op, int pc) {
-	int memory_line = (REG[op->rs] + REG[op->rt]);
-	if (memory_line > 4095 || memory_line < 0) {
-		memory_line = 4095; //do not allow higher addresses 
+int jle(operation* op, int pc) {
+	if (REG[op->src0] <= REG[op->src1]) {
+		REG[7] = pc;
+		return REG[1];
 	}
-	Data_Mem[memory_line] = REG[op->rd];
-	if (op->imm_used) {
-		return pc + 2;
-	}
-	return (pc + 1);
-
+		return (pc + 1);
 }
 
 //18
-int reti(operation* op, int pc) {
-	pc = IOs[7];
-	// zero irq status
-	already_irq = 0;
-	IOs[3] = 0;
-	IOs[4] = 0;
-	IOs[5] = 0;
-	return pc;
+int jeq(operation* op, int pc) {
+	if (REG[op->src0] == REG[op->src1]) {
+		REG[7] = pc;
+		return REG[1];
+	}
+		return (pc + 1);
 }
 
 //19
-int in(operation* op, int pc) {
-	int io_address = REG[op->rs] + REG[op->rt];
-	//int x, y, shade;
-	REG[op->rd] = IOs[io_address];
-
-	//read from monitor_cmd
-	if ((op->rs + op->rd) == 18) {
-		REG[op->rd] = 0;
+int jne(operation* op, int pc) {
+	if (REG[op->src0] != REG[op->src1]) {
+		REG[7] = pc;
+		return REG[1];
 	}
-	//update HwRT
-	fprintf(HwRT, "%u %s %s %08X\n",
-		REG[8], "READ", IO_reg_name(io_address), REG[op->rd]);
-
-	if (op->imm_used) {
-		return pc + 2;
-	}
-	return (pc + 1);
+		return (pc + 1);
 }
 
 //20
-int out(operation* op, int pc) {
-	int x, y, shade;
-	int *dest, *orgn;
-
-	int io_address = REG[op->rs] + REG[op->rt];
-	IOs[io_address] = REG[op->rd];
-
-	//update HwRT
-	fprintf(HwRT, "%u %s %s %08X\n",
-		IOs[8], "WRITE", IO_reg_name(io_address), REG[op->rd]);
-
-	//MONITOR WRITE
-	if ((io_address == 18) && REG[op->rd]) {
-		x = IOs[19]; y = IOs[20]; shade = IOs[21];
-		monitor[y][x] = shade;
-	}
-	//LEDS WRITE
-	else if (io_address == 9) {
-		fprintf(Leds, "%03d %08X\n", IOs[8], REG[op->rd]); //+1
-
-	}
-	//DISK WRITE
-	else if (io_address == 14) { // status
-
-		if (!IOs[17]) { // if disk is free
-			if (IOs[14] == 1) { // if sholud write
-				dest = Disk[IOs[15]];
-				orgn = &Data_Mem[IOs[16]];
-
-				fix_disk_cpy(IOs[15], IOs[16], orgn, dest);
-				mem_copy(orgn, dest);
-			}
-			else  if (IOs[14] == 2) { // if shold read			
-				dest = &Data_Mem[IOs[16]];
-				orgn = Disk[IOs[15]];
-
-				fix_disk_cpy(IOs[15], IOs[16], dest, orgn);
-				mem_copy(orgn, dest);
-			}
-		}
-	}
-
-	if (op->imm_used) {
-		return pc + 2;
-	}
-	return (pc + 1);
+int jin(operation* op, int pc) {
+	REG[7] = pc;
+	return REG[op->src0];
 }
 
 
-
-//21
+//24
 int halt(operation* op, int pc) {
 	printf("  > reached halt function [pc=%d]\n\t", pc);
 	return -1;
-
 }
 
 //assign to $0
 int nop(operation*op, int pc) {
-
 	return pc + 1;
 }
 
