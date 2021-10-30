05d6bc73dc649c2a8503f4ca3195871b20b58d04
Omri Elad <omlad93@gmail.com>
2021-10-30 15:40:02 +0300

    example.bin is clean

--- Lab02/sp.c
+++ Lab02/sp.c
@@ -128,6 +128,21 @@ static char opcode_name[32][4] = {"ADD", "SUB", "LSF", "RSF", "AND", "OR", "XOR"
 				 "HLT", "U", "U", "U", "U", "U", "U", "U"};
 
 
+
+static void dump_sram(sp_t *sp){
+	FILE *fp;
+	int i;
+
+	fp = fopen("sram_out.txt", "w");
+	if (fp == NULL) {
+                printf("couldn't open file sram_out.txt\n");
+                exit(1);
+	}
+	for (i = 0; i < SP_SRAM_HEIGHT; i++)
+		fprintf(fp, "%08x\n", llsim_mem_extract(sp->sram, i, 31, 0));
+	fclose(fp);
+}
+
 /* Op - Codes Functions */
 
 void add_ex0(sp_registers_t *spro, sp_registers_t *sprn) {
@@ -221,53 +236,46 @@ int mem_ex1(sp_registers_t *spro, sp_registers_t *sprn, sp_t* sp ){
 void arithmetics_ex1(sp_registers_t *spro, sp_registers_t *sprn){
 	sprn->r[spro->dst] = spro->aluout;
 	sprn->ctl_state = CTL_STATE_FETCH0; // update machine state
+}
 
+void halt_ex1(sp_t *sp){
+	sp_registers_t *sprn = sp->sprn;
+	sp->start = 0;
+    sprn->ctl_state = CTL_STATE_IDLE;
+    dump_sram(sp);
+    llsim_stop();
 }
 
 void print_trace_file(FILE* trace, sp_registers_t *spro, sp_registers_t *sprn, int data_out){
+		int is_jump = (spro->opcode == JLT || spro->opcode == JLE || spro->opcode == JEQ || spro->opcode == JNE || spro->opcode == JIN);
+		int is_arithmetic = (spro->opcode == ADD || spro->opcode == SUB || spro->opcode == LSF || spro->opcode == RSF || spro->opcode == AND || spro->opcode == OR || spro->opcode == XOR || spro->opcode == LHI);
         fprintf(trace,"--- instruction %i (%04x) @ PC %i (%04x) -----------------------------------------------------------\n",
 				(spro->cycle_counter)/6 -1, (spro->cycle_counter)/6 -1, spro->pc , spro->pc );
         fprintf(trace,"pc = %04d, inst = %08x, opcode = %i (%s), dst = %i, src0 = %i, src1 = %i, immediate = %08x\n",
 				spro->pc , spro->inst,  spro->opcode, opcode_name[spro->opcode], spro->dst, spro->src0, spro->src1, spro->immediate);
         fprintf(trace,"r[0] = 00000000 r[1] = %08x r[2] = %08x r[3] = %08x \nr[4] = %08x r[5] = %08x r[6] = %08x r[7] = %08x \n\n", 
 				(spro->immediate != 0)?spro->immediate:0 , spro->r[2],spro->r[3], spro->r[4], spro->r[5],spro->r[6], spro->r[7]);
-        if (spro->opcode == ADD || spro->opcode == SUB || spro->opcode == LSF || spro->opcode == RSF || spro->opcode == AND || spro->opcode == OR || spro->opcode == XOR || spro->opcode == LHI) {
+        if (is_arithmetic) {
             fprintf(trace,">>>> EXEC: R[%i] = %i %s %i <<<<\n\n", spro->dst, spro->alu0, opcode_name[spro->opcode], spro->alu1);
         } 
 		else if (spro->opcode == LD) 
 		{
-            fprintf(trace,">>>> EXEC: R[%i] = MEM[%i] = %08i <<<<\n\n", spro->dst, (spro->src1 == 1)?spro->immediate:spro->r[spro->src1], data_out);
+            fprintf(trace,">>>> EXEC: R[%i] = MEM[%i] = %08x <<<<\n\n", spro->dst, (spro->src1 == 1)?spro->immediate:spro->r[spro->src1], data_out);
         } 
 		else if (spro->opcode == ST) 
 		{
             fprintf(trace,">>>> EXEC: MEM[%i] = R[%i] = %08x <<<<\n\n", (spro->src1 == 1)?spro->immediate:spro->r[spro->src1], spro->src0, spro->r[spro->src0]);
         } 
-		else if (spro->opcode == JLT || spro->opcode == JLE || spro->opcode == JEQ || spro->opcode == JNE || spro->opcode == JIN) 
+		else if (is_jump) 
 		{
             fprintf(trace,">>>> EXEC: %s %i, %i, %i <<<<\n\n", opcode_name[spro->opcode], spro->r[spro->src0], spro->r[spro->src1], (spro->aluout == 1)?spro->immediate: spro->pc+1);
         } 
 		else if (spro->opcode == HLT) 
 		{
             fprintf(trace, ">>>> EXEC: HALT at PC %04x<<<<\n", spro->pc);
-            fprintf(trace, "sim finished at pc %i, %i instructions\n", spro->pc, (spro->cycle_counter)/6);
+            fprintf(trace, "sim finished at pc %i, %i instructions", spro->pc, (spro->cycle_counter)/6);
         }
     }
-
-static void dump_sram(sp_t *sp)
-{
-	FILE *fp;
-	int i;
-
-	fp = fopen("sram_out.txt", "w");
-	if (fp == NULL) {
-                printf("couldn't open file sram_out.txt\n");
-                exit(1);
-	}
-	for (i = 0; i < SP_SRAM_HEIGHT; i++)
-		fprintf(fp, "%08x\n", llsim_mem_extract(sp->sram, i, 31, 0));
-	fclose(fp);
-}
-
 /*
 	Our Addition
 */
@@ -400,10 +408,8 @@ static void sp_ctl(sp_t *sp)
 
         case CTL_STATE_EXEC1:
             if (spro->opcode == HLT){
-                sp->start = 0;
-                sprn->ctl_state = CTL_STATE_IDLE;
-                dump_sram(sp);
-                llsim_stop();
+				halt_ex1(sp);
+				print_trace_file(inst_trace_fp, spro, sprn, data_out);
                 break;
             } 
 			else if (is_mem){
@@ -462,7 +468,7 @@ static void sp_generate_sram_memory_image(sp_t *sp, char *program_name)
         }
 	sp->memory_image_size = addr;
 
-        fprintf(inst_trace_fp, "program %s loaded, %d lines\n", program_name, addr);        
+        fprintf(inst_trace_fp, "program %s loaded, %d lines\n\n", program_name, addr);        
 
 	for (i = 0; i < sp->memory_image_size; i++)
 		llsim_mem_inject(sp->sram, i, sp->memory_image[i], 31, 0);
