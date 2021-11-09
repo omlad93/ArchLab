425962de6c292a67db72d1007659084a19985a25
Omri Elad <omlad93@gmail.com>
2021-11-09 15:38:06 +0200

    init fifo

--- Lab03/ex8/fifo.v
+++ Lab03/ex8/fifo.v
@@ -2,6 +2,7 @@ module fifo(clk, reset, in, push, pop, out, full);
    parameter N=4; // determines the maximum number of words in queue.
    parameter M=2; // determines the bit-width of each word, stored in the queue.
 
+   localparam ones = ((1 << M) -1);
    input clk, reset, push, pop;
    input [M-1:0] in;
    output [M-1:0] out;
@@ -9,4 +10,66 @@ module fifo(clk, reset, in, push, pop, out, full);
 
    
    // Fill Here
+   
+   reg [M*N-1:0] queue = 0;
+   integer n = 0;   // n==0 means empty queue!
+   reg [M-1:0] word_out = 0;
+   integer is_full = 0;
+   
+	always @(posedge clk or reset) begin
+		if(reset == 1) begin : reset_all
+			queue <= 0;
+			n <= 0;
+			is_full <= 0;
+			word_out = 0; 
+		end
+		else begin : operations
+
+			word_out <=  ((1 << M) - 1) & queue; // create M times '1' in LSB's and padded with 32-M '0' LSB's
+
+			if (n == 0) begin : empty_queue
+				if (push == 1) begin : first_push
+					queue <= (in << (M*n)) | queue ;
+					n <= n + 1;
+				end
+			end
+			else begin
+				if(n < N) begin
+					if (pop == 1 && push == 0) begin : pop_common
+						queue <= (queue >>> M);
+						n<=n-1;
+					end else;
+					if (pop == 0 && push == 1) begin : push_common
+						queue <= (in << (M*n)) | queue ;
+						n<=n+1;
+					end else;
+					if (pop == 1 && push == 1) begin : push_pop_common
+						queue <= (in << (M*(n-1))) | (queue>>>M);
+					end else;
+					if (pop == 0 && push == 0) begin : nop_common
+					end else;
+				end
+				else begin 
+					if(pop == 1 && push == 0) begin : pop_edge
+						queue <= (queue >> M);
+						n <= n - 1;
+					end else;
+					if(pop == 1 && push == 1) begin : push_pop_edge
+						queue <= (in << (M*(n-1))) | (queue >>> M);
+					end else;
+					if(pop == 0) begin : nop_edge
+	
+					end else;
+				end
+			end
+							
+			if(n<N)
+				is_full <= 0;
+			else
+				is_full <= 1;
+		end
+    end
+	
+    assign out = word_out;
+    assign full = is_full; // note that full only updates on posedge clk or reset so full[t+1]='1' iff n[t] == N 
 endmodule
--- Lab03/ex8/fifo_tb.v
+++ Lab03/ex8/fifo_tb.v
@@ -1,13 +1,118 @@
 module main;
-   reg clk, reset, push, pop;
-   reg [2:0] in;
-   wire [2:0] out;
-   wire full;
+reg clk, reset, push, pop;
+reg [1:0] in;
+wire [1:0] out;
+wire full;
+integer fail;
+// Correct the parameter assignment
+fifo #(4,2) fifo_test (clk, reset, in, push, pop, out, full);
 
-   // Correct the parameter assignment
-   fifo uut #(?,?) (clk, reset, in, push, pop, out, full);
+    always@(out or full)
+        begin
+			if (pop & full) $display("time=%d: popped %b from fifo. fifo is full", $time, out);
+			if (pop & ~full) $display("time=%d: popped %b from fifo. fifo is not full", $time, out);
+			if (push & full) $display("time=%d: pushed %b to fifo. fifo is full", $time, in);
+			if (push & ~full) $display("time=%d: pushed %b to fifo. fifo is full", $time, in);
+        end
 
-   always #5 clk = ~clk;
+initial begin
+$dumpfile("waves.vcd");
+$dumpvars;
+clk = 1; fail = 0;
+#10
+//reset params
+reset = 1;
+in = 0;
+push = 0; pop = 0; 
+#10
+if (full != 0 || out != 0) fail <= 1;
 
-   // Fill Here
+#10
+// push 3
+in = 3;
+push = 1;
+#10
+if (full != 0 || out != 0) fail <= 2;
+
+#10 
+// pop 3
+in = 0; push = 0; pop = 1; 
+#10
+if (full != 0 || out != 3) fail <= 3;
+
+#10
+// push 3 (1)
+in = 3;
+#10
+if (full != 0 || out != 0) fail <= 4;
+
+#10
+// push 2 (2)
+in = 2;
+#10
+if (full != 0 || out != 0) fail <= 5;
+
+#10
+// push 1 (3)
+in = 1;
+#10
+if (full != 0 || out != 0) fail <= 6;
+
+#10
+// push 3 (4)
+in = 3;
+#10
+if (full != 1 || out != 0) fail <= 7;
+
+#10
+// push 0 (5 - no possible)
+in = 3;
+#10
+if (full != 1 || out != 0) fail <= 7;
+
+#10
+// push 1 & pop 3 
+pop = 1;
+in = 1;
+#10
+if (full != 1 || out != 1) fail <= 8;
+
+#10
+// pop 1 
+push = 0;
+#10
+if (full != 0 || out != 1) fail <= 9;
+
+
+#10
+// reset it again
+reset = 0 ;
+pop = 0; push = 0;
+in = 0;
+#10
+if (full != 0 || out != 0) fail <= 10;
+
+#10
+// push 3 (1)
+push = 1;
+in = 3;
+#10
+if (full != 1 || out != 0) fail <= 11;
+
+#10
+// pop 3
+push = 0; pop = 1;
+in = 0;
+#10
+if (full != 1 || out != 3) fail <= 11;
+
+#10 
+if(fail == 0)
+	$display("PASSED ALL TESTS");
+else
+	$display("DID NOT PASS ALL TESTS. last failed in %d:", fail);
+	
+$finish;
+end
+always #5 clk = ~clk;
 endmodule
