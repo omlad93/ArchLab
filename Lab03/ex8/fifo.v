d8bb21dc3c1f41877a4a04f0eda3d2b7142565bc
Omri Elad <omlad93@gmail.com>
2021-11-11 07:59:16 +0200

    fifo progress

--- Lab03/ex8/fifo.v
+++ Lab03/ex8/fifo.v
@@ -1,22 +1,24 @@
-module fifo(clk, reset, in, push, pop, out, full);
+module fifo(clk, reset, in, push, pop, out, full, debug_queue);
    parameter N=4; // determines the maximum number of words in queue.
    parameter M=2; // determines the bit-width of each word, stored in the queue.
 
-   localparam ones = ((1 << M) -1);
+   
    input clk, reset, push, pop;
    input [M-1:0] in;
    output [M-1:0] out;
+   output [M*N-1:0] debug_queue;
    output full;
 
    
    // Fill Here
    
    reg [M*N-1:0] queue = 0;
+
    integer n = 0;   // n==0 means empty queue!
    reg [M-1:0] word_out = 0;
    integer is_full = 0;
    
-	always @(posedge clk or reset) begin
+	always @(posedge clk or reset) begin : active
 		if(reset == 1) begin : reset_all
 			queue <= 0;
 			n <= 0;
@@ -25,7 +27,7 @@ module fifo(clk, reset, in, push, pop, out, full);
 		end
 		else begin : operations
 
-			word_out <=  (ones & queue; // create M times '1' in LSB's and padded with 32-M '0' LSB's
+			word_out <=  (((1 << M) -1) & queue); // create M times '1' in LSB's and padded with 32-M '0' LSB's
 
 			if (n == 0) begin : empty_queue
 				if (push == 1) begin : first_push
@@ -37,16 +39,19 @@ module fifo(clk, reset, in, push, pop, out, full);
 				if(n < N) begin
 					if (pop == 1 && push == 0) begin : pop_common
 						queue <= (queue >>> M);
-						n<=n-1;
+						n <= n - 1;
 					end else;
 					if (pop == 0 && push == 1) begin : push_common
 						queue <= (in << (M*n)) | queue ;
-						n<=n+1;
+						n <= n + 1;
 					end else;
 					if (pop == 1 && push == 1) begin : push_pop_common
-						queue <= (in << (M*(n-1))) | (queue>>>M);
+						queue <= (in << (M*(n-1))) | (queue >>> M);
+						n <= n;
 					end else;
 					if (pop == 0 && push == 0) begin : nop_common
+						n <= n;
+						queue <= queue;
 					end else;
 				end
 				else begin 
@@ -56,20 +61,21 @@ module fifo(clk, reset, in, push, pop, out, full);
 					end else;
 					if(pop == 1 && push == 1) begin : push_pop_edge
 						queue <= (in << (M*(n-1))) | (queue >>> M);
+						n <= n;
 					end else;
 					if(pop == 0) begin : nop_edge
-	
+						n <= n;
+						queue <= queue;
 					end else;
 				end
 			end
-							
-			if(n<N)
-				is_full <= 0;
-			else
-				is_full <= 1;
+
+			is_full = (n < N - 1) ? 0 :1;				
+			
 		end
     end
 	
+	assign debug_queue = queue;
     assign out = word_out;
     assign full = is_full; // note that full only updates on posedge clk or reset so full[t+1]='1' iff n[t] == N 
 endmodule
--- Lab03/ex8/fifo_tb.v
+++ Lab03/ex8/fifo_tb.v
@@ -2,22 +2,23 @@ module main;
 reg clk, reset, push, pop;
 reg [1:0] in;
 wire [1:0] out;
+wire [3:0] queue;
 wire full;
-integer fail;
-// Correct the parameter assignment
-fifo #(4,2) fifo_test (clk, reset, in, push, pop, out, full);
+integer fail,print;
 
-    always@(out or full)
-        begin
-			if (pop & full) $display("time=%d: popped %b from fifo. fifo is full", $time, out);
-			if (pop & ~full) $display("time=%d: popped %b from fifo. fifo is not full", $time, out);
-			if (push & full) $display("time=%d: pushed %b to fifo. fifo is full", $time, in);
-			if (push & ~full) $display("time=%d: pushed %b to fifo. fifo is full", $time, in);
-        end
+
+fifo #(4,2) fifo_test (clk, reset, in, push, pop, out, full, queue);
+
+always@(print)
+    begin
+		if (pop) $display("time=%d: popped %b from fifo. fifo is full ? %b\n\t\t\t> q: %b", $time, out, full,queue);
+		if (push) $display("time=%d: pushed %b to fifo. fifo is full ? %b\n\t\t\t> q: %b", $time, in, full, queue);
+    end
 
 initial begin
 $dumpfile("waves.vcd");
 $dumpvars;
+print = 0;
 clk = 1; fail = 0;
 #10
 //reset params
@@ -25,6 +26,7 @@ reset = 1;
 in = 0;
 push = 0; pop = 0; 
 #10
+print = print +1;
 if (full != 0 || out != 0) fail <= 1;
 
 #10
@@ -32,42 +34,51 @@ if (full != 0 || out != 0) fail <= 1;
 in = 3;
 push = 1;
 #10
+print = print +1;
 if (full != 0 || out != 0) fail <= 2;
 
 #10 
 // pop 3
 in = 0; push = 0; pop = 1; 
 #10
+print = print +1;
 if (full != 0 || out != 3) fail <= 3;
 
 #10
 // push 3 (1)
+pop = 0; push =1;
 in = 3;
 #10
+print = print +1;
 if (full != 0 || out != 0) fail <= 4;
 
 #10
 // push 2 (2)
 in = 2;
 #10
+print = print +1;
 if (full != 0 || out != 0) fail <= 5;
 
 #10
 // push 1 (3)
 in = 1;
 #10
+
+print = print +1;
 if (full != 0 || out != 0) fail <= 6;
 
 #10
 // push 3 (4)
 in = 3;
 #10
+print = print +1;
 if (full != 1 || out != 0) fail <= 7;
 
 #10
 // push 0 (5 - no possible)
 in = 3;
 #10
+print = print +1;
 if (full != 1 || out != 0) fail <= 7;
 
 #10
@@ -75,12 +86,14 @@ if (full != 1 || out != 0) fail <= 7;
 pop = 1;
 in = 1;
 #10
+print = print +1;
 if (full != 1 || out != 1) fail <= 8;
 
 #10
 // pop 1 
 push = 0;
 #10
+print = print +1;
 if (full != 0 || out != 1) fail <= 9;
 
 
@@ -90,6 +103,7 @@ reset = 0 ;
 pop = 0; push = 0;
 in = 0;
 #10
+print = print +1;
 if (full != 0 || out != 0) fail <= 10;
 
 #10
@@ -97,14 +111,16 @@ if (full != 0 || out != 0) fail <= 10;
 push = 1;
 in = 3;
 #10
-if (full != 1 || out != 0) fail <= 11;
+print = print +1;
+if (full != 0 || out != 0) fail <= 11;
 
 #10
 // pop 3
 push = 0; pop = 1;
 in = 0;
 #10
-if (full != 1 || out != 3) fail <= 11;
+print = print +1;
+if (full != 0 || out != 3) fail <= 12;
 
 #10 
 if(fail == 0)
