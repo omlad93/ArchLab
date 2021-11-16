ad4e1574006b017944c4b4c8d6786d08744340da
Omri Elad <omlad93@gmail.com>
2021-11-16 09:44:53 +0200

    linux override local

--- Lab03/ex7/sat_count_tb.v
+++ Lab03/ex7/sat_count_tb.v
@@ -1,7 +1,7 @@
 module main;
 
 reg clk, reset, branch, taken;
-reg ans, fail;
+reg ans;
 wire prediction;
 
 sat_count counter(clk, reset, branch, taken, prediction);
@@ -13,7 +13,6 @@ initial begin
 	$dumpvars;
     clk = 0;
     ans = 1;
-    fail = 1;
     branch = 0;
     taken = 0;
 
@@ -23,7 +22,6 @@ initial begin
 
     // FIRST TEST
     ans = ans & (~prediction); //prediction should be 0 --> ans should be 1
-    fail = fail & ans;
     if (ans) begin
         $display("PASS first test");
     end
@@ -41,8 +39,6 @@ initial begin
     ans = ans & (prediction); //prediction should be 1 --> ans should be 1
     #20;
     ans = ans & (prediction); //prediction should be 1 --> ans should be 1
-
-    fail = fail & ans;
 
     if (ans) begin
         $display("PASS second test");
@@ -62,8 +58,6 @@ initial begin
     #20;
     ans = ans & (~prediction); //prediction should be 0 --> ans should be 1
 
-    fail = fail & ans;
-
     if (ans) begin
         $display("PASS third test");
     end
@@ -74,17 +68,8 @@ initial begin
     ans = 1;
     #20;
     ans = ans & (~prediction); //prediction should be 0 --> ans should be 1
-
-    fail = fail & ans;
-
     if (ans) begin
         $display("PASS fourth test");
-    end
-
-    #10
-
-    if (fail) begin
-        $display("PASSED ALL TESTS");
     end
 
     #10
